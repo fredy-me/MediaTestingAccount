1863366d0d6d2fef85c6ad0b63516a9dc48d1d23
fredy-me <fredymelkizedek@gmail.com>
2025-10-21 11:53:54 +0300

    Comment and description on what doing

--- src/main/java/com/social/media/models/SocialProfile.java
+++ src/main/java/com/social/media/models/SocialProfile.java
@@ -8,6 +8,9 @@ public class SocialProfile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long Id;
+
+    //This annotation for One-to-One relation on Single
+    // class used as One-to-One relation after implemented to another class will change and Be Bidirectional relation
     @OneToOne
     @JoinColumn(name = "social_user")
     private SocialUser user;
